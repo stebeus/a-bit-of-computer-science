01cbab6568b908b3a5196d6547cdfd7757d42d67
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-04 15:30:18 -0300

    feat: create seventh Fibonacci term test

--- src/__tests__/fibonacci.test.js
+++ src/__tests__/fibonacci.test.js
@@ -8,6 +8,10 @@ test("First Fibonacci term is 1", () => {
   expect(findFibonacciTerm(1)).toBe([1]);
 });
 
+test("Seventh Fibonacci term is 13", () => {
+  expect(findFibonacciTerm(7)).toBe([13]);
+});
+
 test("Returns 0 if the input is 1", () => {
   expect(iterateFibonacci(1)).toBe([0]);
 });
