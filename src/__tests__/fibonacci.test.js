77d0955c0e3b47333bebf2f14e62c00b53130017
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-04 14:43:04 -0300

    feat: create  input size of 2 test

--- src/__tests__/fibonacci.test.js
+++ src/__tests__/fibonacci.test.js
@@ -3,3 +3,7 @@ import { expect, test } from "vitest";
 test("Returns 0 if the input is 1", () => {
   expect(iterateFibonacci(1)).toBe([0]);
 });
+
+test("Returns 0 and 1 if the input is 2", () => {
+  expect(iterateFibonacci(1)).toBe([0, 1]);
+});
