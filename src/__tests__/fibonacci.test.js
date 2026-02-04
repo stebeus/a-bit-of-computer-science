2dc86b7e03fa84fd65e97e226ed65eea5b0d7120
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-04 15:42:13 -0300

    fix: unwrap theb brackets in the Fibonacci term tests

--- src/__tests__/fibonacci.test.js
+++ src/__tests__/fibonacci.test.js
@@ -1,15 +1,15 @@
 import { expect, test } from "vitest";
 
 test("Zeroth Fibonacci term is 0", () => {
-  expect(findFibonacciTerm(0)).toBe([0]);
+  expect(findFibonacciTerm(0)).toBe(0);
 });
 
 test("First Fibonacci term is 1", () => {
-  expect(findFibonacciTerm(1)).toBe([1]);
+  expect(findFibonacciTerm(1)).toBe(1);
 });
 
 test("Seventh Fibonacci term is 13", () => {
-  expect(findFibonacciTerm(7)).toBe([13]);
+  expect(findFibonacciTerm(7)).toBe(13);
 });
 
 test("Returns 0 if the input is 1", () => {
