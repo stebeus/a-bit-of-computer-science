0fd927353c02ff123856ac901e1b209a61051e62
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-04 15:29:49 -0300

    feat: create first Fibonacci term test

--- src/__tests__/fibonacci.test.js
+++ src/__tests__/fibonacci.test.js
@@ -4,6 +4,10 @@ test("Zeroth Fibonacci term is 0", () => {
   expect(findFibonacciTerm(0)).toBe([0]);
 });
 
+test("First Fibonacci term is 1", () => {
+  expect(findFibonacciTerm(1)).toBe([1]);
+});
+
 test("Returns 0 if the input is 1", () => {
   expect(iterateFibonacci(1)).toBe([0]);
 });
