da11ccc500332566ab786ad96544bf4e0224b607
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-05 15:36:16 -0300

    feat: create single item array test

--- src/__tests__/merge-sort.test.js
+++ src/__tests__/merge-sort.test.js
@@ -3,3 +3,7 @@ import { expect, test } from "vitest";
 test("Works with empty arrays", () => {
   expect(mergeSort([])).toStrictEqual([]);
 });
+
+test("Works with single item arrays", () => {
+  expect(mergeSort([73])).toStrictEqual([73]);
+});
