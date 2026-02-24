b4ef127a418ee6c92f2417677fcb26442143e9f8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-24 12:11:32 -0300

    test: add unit for linked list length increase

--- src/linked-list/linked-list.test.js
+++ src/linked-list/linked-list.test.js
@@ -1,3 +1,15 @@
 import { describe, expect, it } from "vitest";
+import { LinkedList } from "./linked-list";
 
-describe("LinkedList.append", () => {});
+describe("LinkedList.append", () => {
+  it("increases linked list length by one", () => {
+    // Arrange
+    const list = new LinkedList();
+
+    // Act
+    list.append("foo");
+
+    // Assert
+    expect(list).toHaveLength(1);
+  });
+});
