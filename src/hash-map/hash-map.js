2bbfcfcab02794a981fb1f36bec518564a269d64
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-25 07:32:11 -0300

    feat: define private hash method

--- src/hash-map/hash-map.js
+++ src/hash-map/hash-map.js
@@ -5,6 +5,17 @@ class HashMap {
   constructor() {
     this.buckets = new Array(this.#capacity);
   }
+
+  #hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
 
 const hashMap = new HashMap();
