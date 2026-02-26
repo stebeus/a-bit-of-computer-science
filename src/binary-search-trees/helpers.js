df155ba77953e41880d77b34d870b3e429de1b9f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-26 15:07:28 -0300

    restart binary search tree assignment

--- src/binary-search-trees/helpers.js
+++
@@ -1,9 +0,0 @@
-function prettyPrint(node, prefix = '', isLeft = true) {
-  if (node === null || node === undefined) return;
-
-  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-}
-
-export { prettyPrint };
