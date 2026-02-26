ec5a375a441f113cd1e460ba5c8cd71bf282a8f6
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-26 08:30:57 -0300

    feat: define private method to build tree

--- src/binary-search-trees/tree.js
+++ src/binary-search-trees/tree.js
@@ -1,10 +1,27 @@
 import { prettyPrint } from './helpers.js';
 import { Node } from './node.js';
 
-function buildTree(array) {}
-
 class Tree {
+  #sortArrayToBalancedSearchTree(array, start, end) {
+    if (start > end) return null;
+
+    const half = Math.round((end - start) / 2);
+    const middle = start + half;
+    const root = new Node(array[middle]);
+
+    root.left = this.#sortArrayToBalancedSearchTree(array, start, middle - 1);
+    root.right = this.#sortArrayToBalancedSearchTree(array, middle + 1, end);
+
+    return root;
+  }
+
+  #buildTree(array) {
+    const start = 0;
+    const end = array.length - 1;
+    return this.#sortArrayToBalancedSearchTree(array, start, end);
+  }
+
   constructor(array) {
-    this.root = buildTree(array);
+    this.root = this.#buildTree(array);
   }
 }
