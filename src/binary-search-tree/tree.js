33f094437db86a0a8d7ad5e0fcab0186e0b4dd75
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-26 15:22:52 -0300

    feat: add private method for convertToBalancedSearchTree

--- src/binary-search-tree/tree.js
+++ src/binary-search-tree/tree.js
@@ -1,4 +1,19 @@
+import { Node } from './node.js';
+
 class Tree {
+  #convertToBalancedSearchTree(array, start, end) {
+    if (start > end) return null;
+
+    const half = Math.round((end - start) / 2);
+    const middle = start + half;
+    const root = new Node(array[middle]);
+
+    root.left = this.#convertToBalancedSearchTree(middle, start, middle - 1);
+    root.right = this.#convertToBalancedSearchTree(middle, middle + 1, end);
+
+    return root;
+  }
+
   constructor(array) {
     this.root = array;
   }
