9ca41619363a3ea9625344358119902a6f835b92
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-26 16:34:55 -0300

    refactor: abstract recursed and logged values

--- src/binary-search-tree/utils.js
+++ src/binary-search-tree/utils.js
@@ -1,11 +1,25 @@
 const sortInAscendingOrder = (a, b) => a - b;
 
 function prettyPrint(node, prefix = '', isLeft = true) {
-  if (node === null || node === undefined) return;
+  const emptyNode = node === null || node === undefined;
+  if (emptyNode) return;
 
-  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  const symbol = {
+    blank: '    ',
+    straight: '│   ',
+    left: '└── ',
+    right: '┌── ',
+  };
+
+  const branch = {
+    current: `${prefix}${isLeft ? symbol.left : symbol.right}${node.data}`,
+    left: `${prefix}${isLeft ? symbol.blank : symbol.straight}`,
+    right: `${prefix}${isLeft ? symbol.straight : symbol.blank}`,
+  };
+
+  prettyPrint(node.right, branch.right, false);
+  console.log(branch.current);
+  prettyPrint(node.left, branch.left, true);
 }
 
 export { sortInAscendingOrder, prettyPrint };
