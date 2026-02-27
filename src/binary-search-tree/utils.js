5d59026c97ab28ce1391436c985199be46eb13fa
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-27 06:55:20 -0300

    refactor: simplify abstractions

--- src/binary-search-tree/utils.js
+++ src/binary-search-tree/utils.js
@@ -1,25 +1,16 @@
 const sortInAscendingOrder = (a, b) => a - b;
 
 function prettyPrint(node, prefix = '', isLeft = true) {
-  const emptyNode = node === null || node === undefined;
-  if (emptyNode) return;
+  if (node === null || node === undefined) return;
 
-  const symbol = {
-    blank: '    ',
-    straight: '│   ',
-    left: '└── ',
-    right: '┌── ',
-  };
+  const blank = '    ';
+  const straight = '│   ';
+  const left = '└── ';
+  const right = '┌── ';
 
-  const branch = {
-    current: `${prefix}${isLeft ? symbol.left : symbol.right}${node.data}`,
-    left: `${prefix}${isLeft ? symbol.blank : symbol.straight}`,
-    right: `${prefix}${isLeft ? symbol.straight : symbol.blank}`,
-  };
-
-  prettyPrint(node.right, branch.right, false);
-  console.log(branch.current);
-  prettyPrint(node.left, branch.left, true);
+  prettyPrint(node.right, `${prefix}${isLeft ? straight : blank}`, false);
+  console.log(`${prefix}${isLeft ? left : right}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? blank : straight}`, true);
 }
 
 export { sortInAscendingOrder, prettyPrint };
