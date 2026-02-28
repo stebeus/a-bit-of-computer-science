83381279a9c924f5e029437d9e73c0b851df2b06
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-28 14:07:25 -0300

    feat: add method for appending values

--- src/linked-list/linked-list.js
+++ src/linked-list/linked-list.js
@@ -1,6 +1,24 @@
+import { Node } from './node.js';
+
 class LinkedList {
   head = null;
   length = 0;
+
+  append(value) {
+    const node = new Node(value);
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) current = current.next;
+
+    current.next = node;
+  }
 }
 
 // Driver script
