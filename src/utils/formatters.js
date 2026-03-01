2179edaada1c955681adcf94a24a03ea6f5e3e5f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-01 15:06:29 -0300

    feat: add pretty print utility

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,4 +2,17 @@ const formatArray = (array) => array.join(', ');
 
 const formatHashMapEntries = ({ key, value }) => `[${key}, ${value}]`;
 
-export { formatArray, formatHashMapEntries };
+function prettyPrint(node, prefix = '', isLeft = true) {
+  if (node === null || node === undefined) return;
+
+  const blank = '    ';
+  const straight = '│   ';
+  const left = '└── ';
+  const right = '┌── ';
+
+  prettyPrint(node.right, `${prefix}${isLeft ? straight : blank}`, false);
+  console.log(`${prefix}${isLeft ? left : right}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? blank : straight}`, true);
+}
+
+export { formatArray, formatHashMapEntries, prettyPrint };
