1a87b8de304ca0341fcebeb7dccbd4aeae2abd42
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-01 12:57:12 -0300

    refactor!: set up finalized driver script

--- src/hash-map/hash-map-driver.js
+++ src/hash-map/hash-map-driver.js
@@ -1,8 +1,32 @@
+import { formatArray, formatHashMapEntries } from '../utils/formatters.js';
 import { HashMap } from './hash-map.js';
 
 const hashMap = new HashMap();
 
-hashMap.set('John Doe', 'Non-existent person');
+// Insertion
 
-console.log(hashMap.buckets);
-console.log(hashMap.length);
+hashMap.set('John Doe', 'Non-existent person');
+hashMap.set('Item', 'The real item');
+hashMap.set('mIte', 'The fake item');
+hashMap.set('One', 1);
+hashMap.set('Boolean', false);
+
+// Deletion
+
+hashMap.remove('Boolean');
+
+// Formatted outputs
+
+const formattedEntries = hashMap.map(formatHashMapEntries);
+const formattedHashMaps = formatArray(formattedEntries);
+const formattedKeys = formatArray(hashMap.keys());
+const formattedValues = formatArray(hashMap.values());
+
+// Outputs
+
+console.log(`Hash map (${hashMap.length}): ${formattedHashMaps}`);
+console.log(`Keys: ${formattedKeys}`);
+console.log(`Values: ${formattedValues}`);
+
+console.log(`Has 'Item': ${hashMap.has('Item')}`);
+console.log(`Has 'Josh': ${hashMap.has('Josh')}`);
